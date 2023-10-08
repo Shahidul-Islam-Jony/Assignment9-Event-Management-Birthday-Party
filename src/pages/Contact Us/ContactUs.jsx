91ccf43d4aec1ad12d843d01fd931c525e2f42ff
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-10-08 12:29:02 +0600

    Contact Us section added

--- src/pages/Contact Us/ContactUs.jsx
+++ src/pages/Contact Us/ContactUs.jsx
@@ -2,7 +2,52 @@
 const ContactUs = () => {
     return (
         <div>
-            <p>Contact us here</p>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row w-full justify-between">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Contact Information</h1>
+                        <p className="py-6">Thank you for choosing our event..</p>
+                        <p className="text-xl font-bold">Mobile: +880748755588</p>
+                        <p className="text-xl font-bold">Email: birthday@gmail.com</p>
+                    </div>
+
+                    <div className="w-1/2 flex flex-col justify-end">
+                        <h2 className="mb-4 text-5xl font-bold">Contact Us</h2>
+                        <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+                            <form className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="Name" placeholder="Your name" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" placeholder="email" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Phone</span>
+                                    </label>
+                                    <input type="text" placeholder="Your Phone no." className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Message</span>
+                                    </label>
+                                    <textarea className="textarea textarea-bordered resize-none h-36" placeholder="Type your Message"></textarea>
+                                </div>
+
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
